d86367f2f72404770afe5b267ee1ab4323614dc8
djaras <duvan318@gmail.com>
2020-12-18 15:08:10 -0500

    Scraper completado para perfil de platzi. Adición de librerías re y json.

--- scraper/config.py
+++ scraper/config.py
@@ -7,6 +7,10 @@ site_config = {
     'name': 'Platzi',
     'domain': 'https://platzi.com',
     'url_search': 'https://platzi.com/p/$username$',
-    'queries': {},
+    'queries': {
+        'container_elements': 'script',
+        'regex_filter': 'courses: \[.*\]',
+        'regex_group': 0
+    },
     'success_http_status_code': 200
 }
--- scraper/functions.py
+++ scraper/functions.py
@@ -5,6 +5,8 @@ Functions file that supports scraper.
 # python imports
 import requests
 import bs4
+import re
+import json
 
 # scraper imports
 from config import site_config
@@ -26,7 +28,20 @@ def get_student_courses(platzi_username, response):
     
     if response_link.status_code != site_config['success_http_status_code']:
         response['message'] = f'Fail. Status code: {response_link.status_code}'
+    
+    ''' Get courses. '''
+    soup = bs4.BeautifulSoup(response_link.text, 'html.parser')
+    elements = soup.select(queries['container_elements'])
 
+    # find html element with dourses data
+    for element in elements:
+        # use regex to get string with courses
+        str_courses = re.search(queries['regex_filter'], str(element))
+        if str_courses is not None:
+            # string processing to return courses in json format
+            courses = str_courses.group(queries['regex_group'])
+            courses = courses.replace('courses: ','')
+            response['courses'] = json.loads(courses)
+    
     return response
-    
-    
+  
