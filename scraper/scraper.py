163142974b87e80fac70c109d75c6befdc5a0014
djaras <duvan318@gmail.com>
2020-12-18 20:27:43 -0500

    Implementación de Flask para consumo de API

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -11,23 +11,18 @@ import sys
 from functions import get_student_courses
 from config import ok_response
 
-response = {
-    'message': '',
-    'courses': []
-}
 
+def scrape_student_profile(username):
+    response = {
+        'message': '',
+        'courses': []
+    }
 
-# Main
-if __name__ == "__main__":
-    try:
-        platzi_username = sys.argv[1].lower()
-    except IndexError as e:
-        response['message'] = 'Error starting scraping. Username is required'
-    
-    search_result = get_student_courses(platzi_username, response)
+    search_result = get_student_courses(username, response)
 
     if not search_result['courses'] and search_result['message'] == ok_response:
         response['message'] = 'Profile is private or has not approved courses'
-    
-    print(response)
-    
+
+    return(response)
+
+   
